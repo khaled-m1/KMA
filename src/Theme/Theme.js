87a4b290e69147c3b0b445d7da80425c43988c22
khaled-m1 <alanziq00@gmail.com>
2024-09-13 12:31:54 +0300

    fix font

--- src/Theme/Theme.js
+++ src/Theme/Theme.js
@@ -2,9 +2,12 @@ import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
   fonts: {
-    heading: 'tajawal',
-    body: 'tajawal',
+    heading: `'Tajawal', sans-serif`,
+    body: `'Tajawal', sans-serif`,
   },
+  fontFallbacks: {
+    sansSerif: `'Tajawal', 'Helvetica Neue', Arial, sans-serif`,
+  }
 });
 
 export default theme;
